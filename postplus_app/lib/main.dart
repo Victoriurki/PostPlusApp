855ded5ba0d0999ecddb5a06123ed1ef991b734d
EricFrancischett <ericmellof@gmail.com>
2022-05-04 15:40:38 -0300

    Firebase setup implemented

--- postplus_app/lib/main.dart
+++ postplus_app/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:postplus_app/my_app.dart';
 
-Future main() async {  
+void main() async {  
   WidgetsFlutterBinding.ensureInitialized();  
   await Firebase.initializeApp();
   runApp(const MyApp());
