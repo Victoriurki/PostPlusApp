117b50ea99c8d9a017e97314c007633391963179
Victoriurki <101892924+Victoriurki@users.noreply.github.com>
2022-05-16 23:17:00 -0300

    Final changes in cached net image

--- postplus_app/lib/my_widgets/my_user_card.dart
+++ postplus_app/lib/my_widgets/my_user_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import '../my_profie_page/my_profile_page.dart';
 import '../user_model/user_model.dart';
@@ -6,11 +7,11 @@ class MyUserCard extends StatelessWidget {
   final UserModel selectedUserModel;
   final UserModel currentUserModel;
 
-  const MyUserCard(
-      {Key? key,
-      required this.currentUserModel,
-      required this.selectedUserModel,})
-      : super(key: key);
+  const MyUserCard({
+    Key? key,
+    required this.currentUserModel,
+    required this.selectedUserModel,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +27,22 @@ class MyUserCard extends StatelessWidget {
           ),
         );
       },
-      title: Text("${(selectedUserModel.firstName)} ${(selectedUserModel.lastName)}"),
+      title: Text(
+          "${(selectedUserModel.firstName)} ${(selectedUserModel.lastName)}"),
       subtitle: Text('${selectedUserModel.username}'),
-      leading: Container(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage(selectedUserModel.profilePicture!),
+      leading: CachedNetworkImage(
+        imageUrl: selectedUserModel.profilePicture!,
+        progressIndicatorBuilder: (context, url, downloadProgress) =>
+            CircularProgressIndicator(value: downloadProgress.progress),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+        imageBuilder: (context, imageProvider) => Container(
+          width: 40.0,
+          height: 40.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
           ),
         ),
-        width: 40,
-        height: 40,
       ),
     );
   }
